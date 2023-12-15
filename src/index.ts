e6868bd90cbd9ccb50b667f88f5344e3ef77b36a
John Olmsted <john@olmsted.io>
2023-12-15 15:51:22 -0500

    Add initial version of 'ValueMap' class

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,57 @@
-export const hello = "Hello, world!";
+import stringify from "fast-json-stable-stringify";
+
+export class ValueMap<K, V> extends Map<K | string, V> {
+  override get(key: K): V | undefined {
+    return super.get(serializeKey(key));
+  }
+
+  override set(key: K, value: V): this {
+    return super.set(serializeKey(key), value);
+  }
+
+  override has(key: K): boolean {
+    return super.has(serializeKey(key));
+  }
+
+  override delete(key: K): boolean {
+    return super.delete(serializeKey(key));
+  }
+
+  override forEach(
+    callbackfn: (value: V, key: K, map: Map<K, V>) => void,
+    thisArg?: any
+  ): void {
+    super.forEach((value: V, key: string | K, map: Map<string | K, V>) => {
+      callbackfn(value, deserializeKey<K>(key as string), map as Map<K, V>);
+    }, thisArg);
+  }
+
+  override keys(): IterableIterator<K> {
+    const serializedKeys = [...super.keys()] as string[];
+    const deserializedKeys = serializedKeys.map((key) =>
+      deserializeKey<K>(key)
+    );
+
+    return deserializedKeys[Symbol.iterator]();
+  }
+
+  override entries(): IterableIterator<[K, V]> {
+    const serializedEntries = [...super.entries()] as [string, V][];
+    const deserializedEntries = serializedEntries.map((entry) => [
+      deserializeKey<K>(entry[0]),
+      entry[1],
+    ]);
+
+    return deserializedEntries[Symbol.iterator]() as IterableIterator<[K, V]>;
+  }
+}
+
+/** Serializes a `key` into a stable representation so we can key anything by value. */
+function serializeKey<K>(key: K): string {
+  return stringify(key);
+}
+
+/** Deserializes a `key` string back to its original value. */
+function deserializeKey<K>(key: string): K {
+  return JSON.parse(key) as K;
+}
--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -1,7 +1,55 @@
-import { hello } from "..";
+import { ValueMap } from "..";
 
-describe("initial tests", () => {
-  it("works as expected", () => {
-    expect(hello).toEqual("Hello, world!");
+describe("ValueMap", () => {
+  let map: Map<
+    string | Record<string | number | symbol, unknown> | any[],
+    number
+  >;
+
+  const scalar = "foo";
+
+  const object = {
+    neat: "cool",
+    number: 100,
+    nested: {
+      boolean: true,
+    },
+  };
+  const objectWithSameValue = {
+    neat: "cool",
+    number: 100,
+    nested: {
+      boolean: true,
+    },
+  };
+
+  const array = ["cat", "dog", "monkey"];
+  const arrayWithSameValue = ["cat", "dog", "monkey"];
+
+  beforeEach(() => {
+    map = new ValueMap();
+  });
+
+  describe("set", () => {
+    describe("with scalar keys", () => {
+      it("sets a value in the map", () => {
+        map.set(scalar, 1);
+        expect(map.get(scalar)).toEqual(1);
+      });
+    });
+
+    describe("with object keys", () => {
+      it("sets a value in the map", () => {
+        map.set(object, 1);
+        expect(map.get(objectWithSameValue)).toEqual(1);
+      });
+    });
+
+    describe("with array keys", () => {
+      it("sets a value in the map", () => {
+        map.set(array, 1);
+        expect(map.get(arrayWithSameValue)).toEqual(1);
+      });
+    });
   });
 });
