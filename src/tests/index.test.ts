63fd4cccdd886a9dbaa366ac15b9209463dc2a27
John Olmsted <john@olmsted.io>
2023-12-17 12:31:39 -0500

    DRY up existing tests and add tests for 'has'

--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -6,49 +6,69 @@ describe("ValueMap", () => {
     number
   >;
 
-  const scalar = "foo";
-
-  const object = {
-    neat: "cool",
-    number: 100,
-    nested: {
-      boolean: true,
+  const testCases = {
+    scalar: {
+      key: "foo",
+      sameValueKey: "foo",
     },
-  };
-  const objectWithSameValue = {
-    neat: "cool",
-    number: 100,
-    nested: {
-      boolean: true,
+    object: {
+      key: {
+        neat: "cool",
+        number: 100,
+        nested: {
+          boolean: true,
+        },
+      },
+      sameValueKey: {
+        neat: "cool",
+        number: 100,
+        nested: {
+          boolean: true,
+        },
+      },
     },
-  };
-
-  const array = ["cat", "dog", "monkey"];
-  const arrayWithSameValue = ["cat", "dog", "monkey"];
+    array: {
+      key: ["cat", "dog", "monkey"],
+      sameValueKey: ["cat", "dog", "monkey"],
+    }
+  }
 
   beforeEach(() => {
     map = new ValueMap();
   });
 
-  describe("set", () => {
-    describe("with scalar keys", () => {
-      it("sets a value in the map", () => {
-        map.set(scalar, 1);
-        expect(map.get(scalar)).toEqual(1);
+  describe("set and get", () => {
+    Object.entries(testCases).forEach(test => {
+      const keyType = test[0];
+      const fixtures = test[1];
+
+      describe(`with ${keyType} keys`, () => {
+        it("sets a value in the map", () => {
+          map.set(fixtures.key, 1);
+          expect(map.get(fixtures.sameValueKey)).toEqual(1);
+        });
+    
+        it("returns `undefined` if the value isn't set", () => {
+          expect(map.get(fixtures.key)).toEqual(undefined);
+        });
       });
     });
+  });
 
-    describe("with object keys", () => {
-      it("sets a value in the map", () => {
-        map.set(object, 1);
-        expect(map.get(objectWithSameValue)).toEqual(1);
-      });
-    });
-
-    describe("with array keys", () => {
-      it("sets a value in the map", () => {
-        map.set(array, 1);
-        expect(map.get(arrayWithSameValue)).toEqual(1);
+  describe("has", () => {
+    Object.entries(testCases).forEach(test => {
+      const keyType = test[0];
+      const fixtures = test[1];
+      
+      describe(`with ${keyType} keys`, () => {
+        it("returns `true` if the key is set", () => {
+          map.set(fixtures.key, 1);
+          expect(map.has(fixtures.key)).toEqual(true);
+        });
+    
+        it("returns `false` if the value is not set", () => {
+          expect(map.has(fixtures.key)).toEqual(false);
+        });
       });
     });
   });
